2a2737f9aa68fd8d8c92fc54fc7f719feadfc2da
Wambasi <60027629+Wambasi@users.noreply.github.com>
2024-04-14 15:25:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,42 +1,61 @@
+// IIFE
 (() => {
-        let toDoListArray = [];
-        const form = document.querySelector(".form");
-        const input = form.querySelector(".form_input");
-        const ul = document.querySelector(".toDoList");
-    
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            let itemID = String(Date.now());
-            let toDoItem = input.ariaValueMax;
-            addItemToDOM(itemID, toDoItem);
-            input.value = "";
-        });
-    
-        ul.addEventListener("click", (e) => {
-            let id = e.target.getAttribute("data.id");
-            if (!id) return
-            removeItemFromDOM(id);
-            removeItemFromArray(id);
-        });
-        function addItemToDOM(itemID,toDoItem) {
-            const li = document.createElement("li");
-            li.setAttribute("data-id", itemID);
-            li.innerText = toDoItem;
-            ul.appendChild(li);
-        }
+  // stating variables
+  let toDoListArray = [];
+  // variables
+  const form = document.querySelector('.form');
+  const input = document.querySelector('.form_input');
+  const ul = document.querySelector('.toDoList');
 
-        function addItemToArray(itemID, toDoItem) {
-            toDoListArray.push({itemID, toDoItem});
-            console.log(toDoListArray);
-        }
+  // event listeners
+  form.addEventListener('submit', (e) => {
+    // prevent default behavior - Page reload
+    e.preventDefault();
+    // give item a unique ID
+    let itemId = String(Date.now());
+    // get/assign input value
+    let toDoItem = input.value;
+    // pass ID and item into functions
+    addItemToDOM(itemId, toDoItem);
+    addItemToArray(itemId, toDoItem);
+    // clear the input box. (this is default behavior but we got rid of that)
+    input.value = '';
+  });
 
-        function removeItemFromDOM(id) {
-            var li = document.querySelector('[data-id="' + id + '"]');
-            ul.removeChild(li);
-        }
+  ul.addEventListener('click', (e) => {
+    let id = e.target.getAttribute('data-id');
+    if (!id) return; // user clicked in something else
+    // pass id through to functions
+    removeItemFromDOM(id);
+    removeItemFromArray(id);
+  });
 
-        function removeItemFromArray(id) {
-            toDoListArray = toDoListArray.filter((item) => item.itemID !== id);
-            console.log(toDoListArray);
-        }
+  function addItemToDOM(itemId, toDoItem) {
+    // create an li
+    const li = document.createElement('li');
+    li.setAttribute('data-id', itemId);
+    // add toDoItem text to li
+    li.innerText = toDoItem;
+    // add li to the DOM
+    ul.appendChild(li);
+  }
+
+  function addItemToArray(itemId, toDoItem) {
+    // add item to array as an object with an ID so we can find and delete it later
+    toDoListArray.push({ itemId, toDoItem });
+    console.log(toDoListArray);
+  }
+
+  function removeItemFromDOM(id) {
+    // get the list item by data ID
+    var li = document.querySelector('[data-id="' + id + '"]');
+    // remove list item
+    ul.removeChild(li);
+  }
+
+  function removeItemFromArray(id) {
+    // create a new toDoListArray with all li's that don't match the ID
+    toDoListArray = toDoListArray.filter((item) => item.itemId !== id);
+    console.log(toDoListArray);
+  }
 })();
